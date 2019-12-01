2e99a670ac97fc1ac33ec92e25853827b38491e0
tsachi nimni <tsachin@sweetinn.com>
2019-12-01 09:02:08 +0200

    Add comment

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 // routes
 app.use('/api/v1/logs', logRoutes.api);
 
+// Start listen to new containers
 setInterval(dockerOperations.autoLogger, 3000);
 
 app.listen(3000, () => `started listening on port 3000`);
